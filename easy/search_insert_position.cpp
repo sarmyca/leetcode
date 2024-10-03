85e9c9a77ca1a9dbe1500412a33c0c982c03c9e4
Marin Bošković <marinboskovic0@gmail.com>
2024-10-03 21:49:24 +0200

    Update search_insert_position.cpp

--- easy/search_insert_position.cpp
+++ easy/search_insert_position.cpp
@@ -16,19 +16,19 @@ public:
         // While we haven't found the target index (pointers havent crossed each other)
         while (left <= right) {
             // Calculate the middle index -> use it to compare the target with the middle element
-            int mid = left + (right - left) / 2;
+            int middle = left + (right - left) / 2;
 
             // If the middle element is the target -> we return the index
-            if (nums[mid] == target) {
-            return mid;
+            if (nums[middle] == target) {
+            return middle;
 
             // If the middle element is smaller than the target -> we move the left pointer to the right of the middle element
-            } else if (nums[mid] < target) {
-            left = mid + 1;
+            } else if (nums[middle] < target) {
+            left = middle + 1;
 
             // If the middle element is greater than the target -> we move the right pointer to the left of the middle element
             } else {
-            right = mid - 1;
+            right = middle - 1;
             }
         }
         return left;
