961bef085c6004971b0dfcf31da474126ed20458
Marin Bošković <marinboskovic0@gmail.com>
2024-10-03 13:00:26 +0200

    Create make_sum_divisible_by_p.py

---
+++ medium/make_sum_divisible_by_p.py
@@ -0,0 +1,10 @@
+# Leetcode problem: 1590. Make Sum Divisible by P
+# Problem link: https://leetcode.com/problems/make-sum-divisible-by-p/
+
+class Solution(object):
+    def minSubarray(self, nums, p):
+        """
+        :type nums: List[int]
+        :type p: int
+        :rtype: int
+        """
