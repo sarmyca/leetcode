eff31ad2b679e2bea8532af460261ede7158e792
Marin Bošković <marinboskovic0@gmail.com>
2024-10-03 21:48:43 +0200

    Create search_insert_position.cpp

---
+++ easy/search_insert_position.cpp
@@ -0,0 +1,36 @@
+// Leetcode problem: 35. Search Insert Position
+// Problem link: https://leetcode.com/problems/search-insert-position/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Solution using binary search -> O(log n)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        // Setup left and right pointers -> we will use them to find the target index
+        int left = 0, right = nums.size() - 1;
+
+        // While we haven't found the target index (pointers havent crossed each other)
+        while (left <= right) {
+            // Calculate the middle index -> use it to compare the target with the middle element
+            int mid = left + (right - left) / 2;
+
+            // If the middle element is the target -> we return the index
+            if (nums[mid] == target) {
+            return mid;
+
+            // If the middle element is smaller than the target -> we move the left pointer to the right of the middle element
+            } else if (nums[mid] < target) {
+            left = mid + 1;
+
+            // If the middle element is greater than the target -> we move the right pointer to the left of the middle element
+            } else {
+            right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
