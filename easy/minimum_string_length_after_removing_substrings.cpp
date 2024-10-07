5070c818ca560009279f9abb508961fc6db7f3ba
Marin Bošković <marinboskovic0@gmail.com>
2024-10-07 16:03:47 +0200

    Create minimum_string_length_after_removing_substrings.cpp

---
+++ easy/minimum_string_length_after_removing_substrings.cpp
@@ -0,0 +1,20 @@
+// Leetcode problem: 2696. Minimum String Length After Removing Substrings
+// Problem link: https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minLength(string s) {
+        int position;
+
+        // Find the position of "AB" or "CD" -> erase them -> repeat until there is no "AB" or "CD"
+        while ((position = s.find("AB")) != string::npos || (position = s.find("CD")) != string::npos) {
+            s.erase(position, 2);
+        }
+        
+        return s.length();
+    }
+};
